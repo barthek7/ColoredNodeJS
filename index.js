0384c3a7d5a6cd81274003eb69583e858f2f5841
Bartłomiej Trzonek <barthek7@gmail.com>
2018-06-12 21:49:34 +0200

    Add event emitter to index

--- index.js
+++ index.js
@@ -1,10 +1,23 @@
+var EventEmitter = require('events').EventEmitter;
 var OSinfo = require('./modules/OSinfo');
+
+var emitter = new EventEmitter();
+
+emitter.on('beforeCommand', function(instruction){
+    console.log('You wrote: ' + instruction + ' trying to run commnad.');
+
+});
+
+emitter.on('afterCommand', function(){
+    console.log('Finished command.');
+});
 
 process.stdin.setEncoding('utf-8');
 process.stdin.on('readable', function(){
     var input = process.stdin.read();
     if(input !== null){
         var instruction = input.toString().trim();
+        emitter.emit('beforeCommand', instruction);
         switch(instruction){
             case '/exit':
                 process.stdout.write('Quitting app!\n');
@@ -27,7 +40,7 @@ process.stdin.on('readable', function(){
             default:
                 process.stderr.write('Wrong instruction!\n');
         }
-
+        emitter.emit('afterCommand');
     }
 
 });
